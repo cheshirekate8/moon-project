e47084c3e5b56983894ce10fd08ebfae18c50054
Katie Young <katie@sandstone.la>
2022-06-09 13:52:51 -0700

    basic app done, now for the bonuses

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+
 
 function App() {
+  const [data, setData] = useState<any>(null);
+  const [showUSD, setShowUSD] = useState<boolean>(false)
+
+  useEffect(() => {
+    fetch("/top20")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const listItems = data?.map((account: any, index: number) =>
+    <>
+      <li hidden={showUSD} className={styles.listItem}><span className={styles.amount}>{account.sol} SOL</span></li>
+      <li hidden={!showUSD} className={styles.listItem}><span className={styles.amount}>${(account.usd).toFixed(2)}</span></li>
+    </>
+  );
+
+  const handleChange = () => {
+    setShowUSD(!showUSD)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <img src="/solanaLogo.svg" alt="Solana logo" className={styles.logo}></img>
+      <div className={styles.top20Div}>
+        <header>Top 20 Accounts</header>
+        <label className={styles.toggleSwitch}>
+          <span className={styles.toggleText}></span>
+          <input type="checkbox" checked={showUSD} onChange={handleChange}></input>
+          <span className={styles.switch} />
+        </label>
+        {
+          data ?
+            <ul className={styles.list}>
+              {listItems}
+            </ul>
+            :
+            <p> Loading... </p>
+        }
+        {/* <button onClick={handleChange} className={styles.convertButton}>Change to {showUSD ? "SOL" : "USD"}</button> */}
+      </div>
     </div>
   );
 }
